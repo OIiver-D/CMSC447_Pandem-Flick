97c1ca16048bf014c35a484ea57da22fd7e1a0d8
OIiver-D <olliedininno@gmail.com>
2021-03-31 14:08:46 -0400

    Added function to get movie id from title

--- BOT_TOKEN.py
+++ BOT_TOKEN.py
@@ -1,2 +1,2 @@
 # Put the discord generated token for a specific bot here
-TOKEN = 
+TOKEN = ODExNDMzMjQ4MDAwOTY2Njg1.YCyIGg.DVerO8J2i6cDevXcB10dQ-ESE68
---
+++ get_id.py
@@ -0,0 +1,19 @@
+import http.client
+import string
+import json
+
+#Sets up HTTPS connection
+def get_id(title):
+    conn = http.client.HTTPSConnection("imdb8.p.rapidapi.com")
+
+    headers = {
+        'x-rapidapi-key': "d8636faa8fmsh783aacbb586ca61p118b3cjsn4260967350bc",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
+        }
+    title=title.replace(" ", "%20")
+    conn.request("GET", "/title/find?q="+title, headers=headers)
+    # This converts json into dictionry to use in python
+    res = conn.getresponse()
+    data = json.loads(res.read().decode("utf-8"))
+    movie_id = data['results'][0]['id'][7:-1]
+    return movie_id
